42242ebcabad1df4dc291fa1d77955263f5df8be
sacki5 <isac.ljung@getaccept.com>
2022-11-07 12:30:08 +0100

    Update

--- histoire.config.ts
+++ histoire.config.ts
@@ -4,7 +4,4 @@ import { HstVue } from '@histoire/plugin-vue'
 export default defineConfig({
   plugins: [HstVue()],
   setupFile: 'histoire.setup.ts',
-  vite: (config, env) => {
-    config.base = env.mode === 'build' ? '/fried-tofu/' : '/'
-  },
 })
